74e480c77eea59d3038c26ec925dbf6e7a65d742
musang33@nate.com <inshalla@hybecorp.com>
2022-12-02 17:39:08 +0900

    1. GetAllBlogPosts 추가 2. service 로직 추가 3. protobuf 생성 파일 위치 지정 추가

--- src/main/java/com/example/songarmeria/grpc/GrpcBlogService.java
+++ src/main/java/com/example/songarmeria/grpc/GrpcBlogService.java
@@ -1,14 +1,19 @@
 package com.example.songarmeria.grpc;
 
-import com.example.songarmeria.blog.grpc.BlogPostM;
-import com.example.songarmeria.blog.grpc.BlogServiceGrpc;
-import com.example.songarmeria.blog.grpc.CreateBlogPostRequest;
+import com.example.blog.grpc.BlogPostM;
+import com.example.blog.grpc.BlogServiceGrpc;
+import com.example.blog.grpc.CreateBlogPostRequest;
+import com.example.blog.grpc.ListBlogPostsResponse;
 import com.example.songarmeria.service.PostsService;
 import com.example.songarmeria.web.dto.PostsRequestDto;
+import com.example.songarmeria.web.dto.PostsUpdateRequestDto;
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.stream.Collectors;
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class GrpcBlogService extends BlogServiceGrpc.BlogServiceImplBase{
@@ -21,7 +26,31 @@ public class GrpcBlogService extends BlogServiceGrpc.BlogServiceImplBase{
         final var id = postsService.save(new PostsRequestDto(request.getTitle(), request.getContent()));
         final var postsResponseDto = postsService.findById(id);
 
+        log.info("createBlogPost");
+        try{
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("createBlogPost end");
+
         responseObserver.onNext(BlogPostM.newBuilder()
+                .setId(postsResponseDto.getId())
+                .setTitle(postsResponseDto.getTitle())
+                .setContent(postsResponseDto.getContent())
+                .setModifiedAt(postsResponseDto.getLastModifiedAt().getSecond())
+                .build());
+
+
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getBlogPost(com.example.blog.grpc.GetBlogPostRequest request,
+                            io.grpc.stub.StreamObserver<com.example.blog.grpc.BlogPostM> responseObserver) {
+        final var postsResponseDto = postsService.findById(request.getId());
+
+        responseObserver.onNext( BlogPostM.newBuilder()
                 .setId(postsResponseDto.getId())
                 .setTitle(postsResponseDto.getTitle())
                 .setContent(postsResponseDto.getContent())
@@ -30,4 +59,58 @@ public class GrpcBlogService extends BlogServiceGrpc.BlogServiceImplBase{
         responseObserver.onCompleted();
     }
 
+    @Override
+    public void getAllBlogPosts(com.google.protobuf.Empty request,
+                                io.grpc.stub.StreamObserver<com.example.blog.grpc.BlogPostM> responseObserver) {
+        final var blogPostMList = postsService.findAll().stream().map(post -> BlogPostM.newBuilder()
+                .setId(post.getId())
+                .setTitle(post.getTitle())
+                .setContent(post.getContent())
+                .setModifiedAt(post.getLastModifiedAt().getSecond())
+                .build()).toList();
+
+        blogPostMList.forEach( blogPostM -> responseObserver.onNext(blogPostM) );
+
+        responseObserver.onCompleted();
+
+
+    }
+
+    @Override
+    public void listBlogPosts(com.example.blog.grpc.ListBlogPostsRequest request,
+                              io.grpc.stub.StreamObserver<com.example.blog.grpc.ListBlogPostsResponse> responseObserver) {
+
+        final var blogPostMList = postsService.findAll().stream().map(post -> BlogPostM.newBuilder()
+                .setId(post.getId())
+                .setTitle(post.getTitle())
+                .setContent(post.getContent())
+                .setModifiedAt(post.getLastModifiedAt().getSecond())
+                .build()).toList();
+        responseObserver.onNext(ListBlogPostsResponse.newBuilder().addAllBlogs(blogPostMList).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void updateBlogPost(com.example.blog.grpc.UpdateBlogPostRequest request,
+                               io.grpc.stub.StreamObserver<com.example.blog.grpc.BlogPostM> responseObserver) {
+        final var postsUpdateRequestDto = new PostsUpdateRequestDto(request.getId(), request.getTitle(), request.getContent());
+        postsService.update(postsUpdateRequestDto);
+
+        final var postsResponseDto = postsService.findById(request.getId());
+
+        responseObserver.onNext( BlogPostM.newBuilder()
+                .setId(postsResponseDto.getId())
+                .setTitle(postsResponseDto.getTitle())
+                .setContent(postsResponseDto.getContent())
+                .setModifiedAt(postsResponseDto.getLastModifiedAt().getSecond())
+                .build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void deleteBlogPost(com.example.blog.grpc.DeleteBlogPostRequest request,
+                               io.grpc.stub.StreamObserver<com.google.protobuf.Empty> responseObserver) {
+        postsService.delete(request.getId());
+        responseObserver.onCompleted();
+    }
 }
--- src/main/java/com/example/songarmeria/web/dto/PostsUpdateRequestDto.java
+++ src/main/java/com/example/songarmeria/web/dto/PostsUpdateRequestDto.java
@@ -1,12 +1,13 @@
 package com.example.songarmeria.web.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public class PostsUpdateRequestDto {
-    private Long id;
-    private String title;
-    private String content;
+    final private Long id;
+    final private String title;
+    final private String content;
 }
